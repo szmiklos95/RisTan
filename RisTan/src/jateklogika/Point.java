53d57398bafa47d57f7bd4fa88701c18895d65d9
PAndras96 <palkoandras96@gmail.com>
2018-03-24 14:51:31 +0100

    Point class added

---
+++ RisTan/src/jateklogika/Point.java
@@ -0,0 +1,20 @@
+package jateklogika;
+
+//A point in the coordinate system
+public class Point{
+	private int i;
+	private int j;
+	
+	public Point(int i,int j){
+		this.i=i;
+		this.j=j;
+	}
+	
+	public int getI() {
+		return i;
+	}
+	
+	public int getJ() {
+		return j;
+	}
+}
