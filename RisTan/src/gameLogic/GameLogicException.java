3d67ecce459ae1b1fbc890a083e6a2e51c254cdd
PAndras96 <palkoandras96@gmail.com>
2018-03-24 15:48:35 +0100

    Removed message from GameLogicExceptions

--- RisTan/src/gameLogic/GameLogicException.java
+++ RisTan/src/gameLogic/GameLogicException.java
@@ -3,8 +3,4 @@ package gameLogic;
 //An exception from the internal game logic
 public class GameLogicException extends Exception {
 	private static final long serialVersionUID=1L;
-
-	public GameLogicException(String message) {
-		super(message);
-	}
 }
--- RisTan/src/gameLogic/InsufficientResourceException.java
+++ RisTan/src/gameLogic/InsufficientResourceException.java
@@ -3,8 +3,4 @@ package gameLogic;
 //The player has not enough resources for something
 public class InsufficientResourceException extends GameLogicException {
 	private static final long serialVersionUID=1L;
-	
-	public InsufficientResourceException(String message) {
-		super(message);
-	}
 }
