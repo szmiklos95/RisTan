7b62ed0c35c6168730ba22d50a498627ce17b540
PAndras96 <palkoandras96@gmail.com>
2018-03-24 15:10:20 +0100

    BuildingLevel enum added

---
+++ RisTan/src/jateklogika/BuildingLevel.java
@@ -0,0 +1,23 @@
+package jateklogika;
+
+enum BuildingLevel{
+	None(1,1),
+	Village(2,4),
+	Town(3,6);
+	
+	private final int resourceNum;
+	private final int score;
+	
+	private BuildingLevel(int resourceNum,int score) {
+		this.resourceNum=resourceNum;
+		this.score=score;
+	}
+	
+	int getResourceNum() {
+		return resourceNum;
+	}
+	
+	int getScore() {
+		return score;
+	}
+}
