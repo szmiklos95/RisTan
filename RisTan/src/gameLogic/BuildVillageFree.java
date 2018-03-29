d8bc7151b537daa78f8b17ac09987916e7278deb
PAndras96 <palkoandras96@gmail.com>
2018-03-29 16:30:52 +0200

    Added class BuildVillageFree

---
+++ RisTan/src/gameLogic/BuildVillageFree.java
@@ -0,0 +1,11 @@
+package gameLogic;
+
+import java.util.HashMap;
+
+public class BuildVillageFree extends BuildVillageAction {
+private static final long serialVersionUID = 1L;
+	
+	BuildVillageFree(int playerID,Point point){
+		super(playerID,0,point,new HashMap<Resource,Integer>());
+	}
+}
