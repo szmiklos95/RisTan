69f20117f6e3c0d3a7ea17204abcf15a7141ee13
PAndras96 <palkoandras96@gmail.com>
2018-03-29 16:04:28 +0200

    Added exception TileIsNotFreeException

---
+++ RisTan/src/gameLogic/TileIsNotFreeException.java
@@ -0,0 +1,6 @@
+package gameLogic;
+
+//a player tries an action on a not free tile which requires a free tile
+public class TileIsNotFreeException extends GameLogicException {
+	private static final long serialVersionUID = 1L;
+}
