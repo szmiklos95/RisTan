05b61928705cd74b517c41c141743558718702af
PAndras96 <palkoandras96@gmail.com>
2018-03-29 14:41:37 +0200

    Added exception PlayerOutOfTurnException

---
+++ RisTan/src/gameLogic/PlayerOutOfTurnException.java
@@ -0,0 +1,6 @@
+package gameLogic;
+
+//a player wants to do something out of his/her turn
+public class PlayerOutOfTurnException extends GameLogicException {
+	private static final long serialVersionUID=1L;
+}
