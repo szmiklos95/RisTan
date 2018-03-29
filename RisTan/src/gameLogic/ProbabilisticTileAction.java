338ace3ee4e0d47d4aedf017f3228d70cf67dc7d
PAndras96 <palkoandras96@gmail.com>
2018-03-29 15:48:31 +0200

    Added class ProbabilisticTileAction

---
+++ RisTan/src/gameLogic/ProbabilisticTileAction.java
@@ -0,0 +1,21 @@
+package gameLogic;
+
+import java.util.Map;
+import java.util.Random;
+
+//a TileAction which has P<1 probability of success
+public abstract class ProbabilisticTileAction extends TileAction {
+	private static final long serialVersionUID = 1L;
+	
+	private boolean successful;
+	
+	ProbabilisticTileAction(int playerID,int time,Point point,Map<Resource,Integer> cost,double probability){
+		super(playerID,time,point,cost);
+		Random rand=new Random();
+		successful=rand.nextDouble()<probability;
+	}
+	
+	boolean isSuccessful() {
+		return successful;
+	}
+}
