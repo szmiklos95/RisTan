f04175f4253ce0430e95d67a8e2969401b447ac4
PAndras96 <palkoandras96@gmail.com>
2018-03-29 15:07:13 +0200

    Added exception NoTileAtPointException

---
+++ RisTan/src/gameLogic/NoTileAtPointException.java
@@ -0,0 +1,6 @@
+package gameLogic;
+
+//a player tries to do something with a tile that not exists
+public class NoTileAtPointException extends GameLogicException {
+	private static final long serialVersionUID = 1L;
+}
