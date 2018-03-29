4e0d4afda48d9315325369b07271290da520429a
PAndras96 <palkoandras96@gmail.com>
2018-03-29 14:03:10 +0200

    Moved the resource probabilites in Board to the generation

--- RisTan/src/gameLogic/Board.java
+++ RisTan/src/gameLogic/Board.java
@@ -9,16 +9,15 @@ import config.Config;
 //The game board
 public class Board {
 	private Map<Point,Tile> tiles;
-	private Map<Resource,Double> res_prob;
 	private Random rand;
 	
-	public Board(Map<Resource,Double> res_prob) {
-		this.res_prob=res_prob;
+	Board() {
+		tiles=null;
 		rand=new Random();
 	}
 	
 	//generates the tile based on the res_prob resource probabilities
-	public void generate() {
+	public void generate(Map<Resource,Double> res_prob) {
 		tiles=new HashMap<Point,Tile>();
 		int size=Config.Board.size;
 		for(int i=-size;i<=size;++i) {
@@ -26,7 +25,7 @@ public class Board {
 				if(Math.abs(i-j)<=size) {
 					Tile tile;
 					do {
-						tile=getNewTile();
+						tile=getNewTile(res_prob);
 					}while(tile==null);
 					Point point=new Point(i,j);
 					tiles.put(point,tile);
@@ -36,7 +35,7 @@ public class Board {
 	}
 	
 	//creates a new tile
-	private Tile getNewTile() {
+	private Tile getNewTile(Map<Resource,Double> res_prob) {
 		double rnd=rand.nextDouble();
 		Resource[] resources=Resource.values();
 		for(int i=0;i<resources.length;++i) {
