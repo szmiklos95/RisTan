4ba740fa7d24f0c3777b91d1bdd90fd496dfdcb4
PAndras96 <palkoandras96@gmail.com>
2018-03-29 13:08:56 +0200

    Added class TradeOffer

---
+++ RisTan/src/gameLogic/TradeOffer.java
@@ -0,0 +1,51 @@
+package gameLogic;
+
+//stores a trade offer
+public class TradeOffer extends ObjectWithID{
+	int offererID;
+	Resource give;
+	int give_amount;
+	Resource take;
+	int take_amount;
+	
+	TradeOffer(int offererID,Resource give,int give_amount,Resource take,int take_amount){
+		init(offererID,give,give_amount,take,take_amount);
+	}
+	
+	TradeOffer(int ID,int offererID,Resource give,int give_amount,Resource take,int take_amount){
+		super(ID);
+		init(offererID,give,give_amount,take,take_amount);
+	}
+	
+	private void init(int offererID,Resource give,int give_amount,Resource take,int take_amount) {
+		this.offererID=offererID;
+		this.give=give;
+		this.give_amount=give_amount;
+		this.take=take;
+		this.take_amount=take_amount;
+	}
+	
+	public int getID() {
+		return super.getID();
+	}
+	
+	public int getOffererID() {
+		return offererID;
+	}
+	
+	public Resource getGive() {
+		return give;
+	}
+	
+	public int getGive_amount() {
+		return give_amount;
+	}
+	
+	public Resource getTake() {
+		return take;
+	}
+	
+	public int getTake_amount() {
+		return take_amount;
+	}
+}
