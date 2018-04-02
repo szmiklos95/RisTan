a10146c4a80398c04ad23565cae37e6f670d8cce
szmiklos95 <sz.miklos95@gmail.com>
2018-04-02 15:35:20 +0200

    Added startAction

--- RisTan/src/graphics/MainMenu.java
+++ RisTan/src/graphics/MainMenu.java
@@ -3,22 +3,43 @@ package graphics;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
+
 public class MainMenu {
+	
+
     public static void addComponentsToPane(Container pane) {
         pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
         
-        pane.setPreferredSize(new Dimension(1024,768));
-        addAButton("Start", pane);
+        pane.setPreferredSize(new Dimension(800,600));
+        
+        JButton button;
+        
+        button =  new JButton("Start");
+        button.setMaximumSize(new Dimension(Integer.MAX_VALUE, button.getMinimumSize().height));
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.addActionListener(new ActionListener() { 
+        	 public void actionPerformed(ActionEvent e) { 
+        	   startAction();
+        	 } 
+        } );
+        pane.add(button);
+        
         addAButton("Button 2", pane);
         addAButton("Button 3", pane);
         addAButton("Long-Named Button 4", pane);
         addAButton("Exit", pane);
       
+    }
+    
+    private static void startAction(){
+    	System.out.print("Hello World!");
     }
 
     private static void addAButton(String text, Container container) {
@@ -27,6 +48,14 @@ public class MainMenu {
         button.setAlignmentX(Component.CENTER_ALIGNMENT);
         container.add(button);
     }
+    
+    private static void setButton(JButton button, String text, Container container) {
+        button = new JButton(text);
+        button.setMaximumSize(new Dimension(Integer.MAX_VALUE, button.getMinimumSize().height));
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        container.add(button);
+    }
+    
 
     /**
      * Create the GUI and show it.  For thread safety,
@@ -45,5 +74,6 @@ public class MainMenu {
         frame.pack();
         frame.setVisible(true);
     }
+
 
 }
