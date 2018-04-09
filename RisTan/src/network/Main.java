d192308eb2d92b861a9b717e51bf12bef05aa0bb
danyipeter <danyi.peter8@gmail.com>
2018-04-09 23:54:27 +0200

    Modified main class - in this version 3 threads can be handled
    - 1. server created then connect
    - 2. players created and connect to server
    - 3. assign the clients to server's clientThreads (with identification
    messages)
    - 4. Send messages

--- RisTan/src/network/Main.java
+++ RisTan/src/network/Main.java
@@ -1,26 +1,36 @@
 package network;
 
+import network.Message.*;
+
 /* This demo application is about sending and receiving
  * an object (TransferClass instance) what is serializable,
  * between a server (System) and a client (Player)*/
 public class Main {
 
 	public static void main(String[] args) {
-		TransferClass packet1 = new TransferClass("...", 1, 1.1);
-		TransferClass packet2 = new TransferClass("---", 200, 2.22);
 		
-		SerialServer system = new SerialServer();
-		system.Connect("localhost");
+		SerialServer rendszer = new SerialServer();
+		rendszer.Connect("localhost");
 		
-		SerialClient player = new SerialClient();
-		player.Connect("localhost");
+		SerialClient andris = new SerialClient();
+		andris.Connect("localhost");
 		
-		player.Send(packet1);
+		SerialClient miki = new SerialClient();
+		miki.Connect("localhost");
 		
-		system.Send(packet2);
+		SerialClient peti = new SerialClient();
+		peti.Connect("localhost");
 		
-		//player.disconnect();
-		//system.disconnect();
+		// Assign the Client and Server's clientThread
+		andris.Send(new Message(eMsgType.Identification, 0)); // player0
+		miki.Send(new Message(eMsgType.Identification, 1));   // player1
+		peti.Send(new Message(eMsgType.Identification, 2));   // player2
+		
+		andris.Send(new Message("Szoftvertechnológia házi feladat"));
+		rendszer.Send(new Message("üzenet a szervertõl a 0. játékosnak"), 0); // rendszer -> andris
+		rendszer.Send(new Message("Példa mondat."), 2); // rendszer -> peti
+		
+		//rendszer.disconnect();
 		
 		
 		
