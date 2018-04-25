a37f9d2004ce1e9b41c7ebbc2d80b18615ed6ecc
PAndras96 <palkoandras96@gmail.com>
2018-04-25 07:42:56 +0200

    Added Javadoc

--- RisTan/src/gameLogic/HarvestResourcesAction.java
+++ RisTan/src/gameLogic/HarvestResourcesAction.java
@@ -1,8 +1,17 @@
 package gameLogic;
 
+/**
+ * Describes an action where the player harvests the resources from his/her tiles. This action requires no time.
+ * @author Andras
+ *
+ */
 public class HarvestResourcesAction extends InGameAction {
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Constructor.
+	 * @param playerID the action executing player.
+	 */
 	public HarvestResourcesAction(int playerID) {
 		super(playerID);
 	}
