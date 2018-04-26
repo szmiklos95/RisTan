5a12ec759f80b16bfa16fffe9160f6da25346200
PAndras96 <palkoandras96@gmail.com>
2018-04-26 07:42:55 +0200

    Added Javadoc

--- RisTan/src/gameLogic/TileAction.java
+++ RisTan/src/gameLogic/TileAction.java
@@ -23,7 +23,7 @@ public abstract class TileAction extends InGameAction {
 	 * Constructor.
 	 * @param playerID the ID of the action executing player.
 	 * @param time the time cost of the action.
-	 * @param point the coordinates of the target tile.
+	 * @param point the coordinates of the target tile (in board coordinate system).
 	 * @param cost the resource cost of the action.
 	 */
 	public TileAction(int playerID,int time,Point point,Map<Resource,Integer> cost){
@@ -32,10 +32,25 @@ public abstract class TileAction extends InGameAction {
 		this.cost=cost;
 	}
 	
+	/**
+	 * Gets the coordinates of the target tile (in board coordinate system).
+	 * @return the coordinates of the target tile (in board coordinate system).
+	 */
 	Point getPoint() {
 		return point;
 	}
 	
+	/**
+	 * Checks whether the action can be executed at a game state.
+	 * It does not change the game state.
+	 * If the action cannot be executed, throws an according GameLogicException.<br>
+	 * If the game is over, throws a GameOverException.<br>
+	 * If the executing player is not the active, throws a PlayerOutOfTurnException.<br>
+	 * If the executing player does not have enough time, throws a NotEnoughTimeException. <br>
+	 * If no tile exists at the target coordinates, throws a NoTileAtPointException. <br>
+	 * If the executing player does not have enough resources, throws an InsufficientResourceException. <br>
+	 * If this tile action is invalid in the actual turn, throws an InvaligTileActionException.
+	 */
 	@Override
 	void check(GameState gameState)throws GameLogicException{
 		super.check(gameState);
@@ -50,6 +65,16 @@ public abstract class TileAction extends InGameAction {
 		}
 	}
 	
+	/**
+	 * Tries to execute the action on a game state.
+	 * If the execution is unsuccessful, the game state remains unchanged and throws a GameLogicException of the cause.<br>
+	 * If the game is over, throws a GameOverException.<br>
+	 * If the executing player is not the active, throws a PlayerOutOfTurnException.<br>
+	 * If the executing player does not have enough time, throws a NotEnoughTimeException. <br>
+	 * If no tile exists at the target coordinates, throws a NoTileAtPointException. <br>
+	 * If the executing player does not have enough resources, throws an InsufficientResourceException. <br>
+	 * If this tile action is invalid in the actual turn, throws an InvaligTileActionException.
+	 */
 	@Override
 	void execute(GameState gameState)throws GameLogicException{
 		super.execute(gameState);
