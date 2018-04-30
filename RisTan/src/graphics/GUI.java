57df8b2ac7d89336e283ebf217bfc1bd5cf0c726
szmiklos95 <sz.miklos95@gmail.com>
2018-04-30 19:19:30 +0200

    Menubar to it's own class

--- RisTan/src/graphics/GUI.java
+++ RisTan/src/graphics/GUI.java
@@ -2,17 +2,10 @@ package graphics;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
-import java.awt.ComponentOrientation;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.Box;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import config.Config;
 import network.SerialClient;
@@ -37,22 +30,7 @@ public class GUI extends JFrame {
 		CardSync.client = new SerialClient();
 		CardSync.controller = CardSync.client.getController();
 		
-		
-		// Set menubar for frame
-		// TODO: Move to a new method
-		// TODO: Confirmation if game started and player wants to return to main menu
-		JMenuBar menubar = new JMenuBar();
-		JMenu file = new JMenu("File");
-		addMenuItem(Config.GUI.ButtonTexts.mainMenu, Config.GUI.CardIDs.mainMenu, file, switchCardAction);
-		addMenuItem(Config.GUI.ButtonTexts.exit, null, file, exitAction);
-		menubar.add(file);
-		menubar.add(Box.createHorizontalGlue());
-		JMenu textItem = new JMenu();
-		textItem.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT); 
-		
-		menubar.add(textItem);
-		
-		CardSync.frame.setJMenuBar(menubar);
+		new GameMenubar(); //Constructor call automatically creates a menu bar
 	}
 
 	/**
@@ -109,66 +87,5 @@ public class GUI extends JFrame {
 	public void refreshGameState(gameLogic.GameState gameState) {
 		CardSync.gameState = gameState;
 	}
-
-	/*
-	 * *****************************************************************************
-	 * 
-	 * @@@@@@@@@@@@@@@@@@@@@@@@@@ Helper (private)
-	 * methods @@@@@@@@@@@@@@@@@@@@@@@@@@
-	 *****************************************************************************/
-
-	// *********** JPanel items ***********//
-
-	/**
-	 * Adds a JMenuItem to a JMenu
-	 * 
-	 * @param text
-	 * @param actionCommand
-	 *            A string stored for firing an action with the action listener.
-	 *            Leave null if not using switchCardAction.
-	 * @param container
-	 * @param f
-	 *            Action listener function
-	 */
-	private void addMenuItem(String text, String actionCommand, Container container, Function f) {
-		JMenuItem menuItem = new JMenuItem(text);
-		menuItem.setActionCommand(actionCommand);
-
-		menuItem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				f.doAction(e);
-			}
-		});
-		container.add(menuItem);
-	}
-
-	// *********** Interface functions ***********//
-	/**
-	 * Interfaces and Actions for button press
-	 */
-	private interface Function {
-		void doAction(ActionEvent e);
-	}
-
-	/**
-	 * Do this when the exit button is pressed
-	 */
-	private final Function exitAction = new Function() {
-		public void doAction(ActionEvent e) {
-			System.out.print("Closing the game.\n");
-			System.exit(0);
-		}
-	};
-
-	/**
-	 * Used for switching between cards (JPanels) on the JFrame
-	 */
-	private final Function switchCardAction = new Function() {
-		public void doAction(ActionEvent e) {
-			CardLayout cl = (CardLayout) (CardSync.cards.getLayout());
-			cl.show(CardSync.cards, e.getActionCommand());
-			System.out.print("Switching to " + e.getActionCommand() + " panel.\n");
-		}
-	};
 
 }
