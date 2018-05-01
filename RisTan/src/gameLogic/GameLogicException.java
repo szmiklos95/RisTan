6a40232cb61fe6f05257b31fd086a1506ffcef63
PAndras96 <palkoandras96@gmail.com>
2018-05-01 13:44:26 +0200

    Added Javadoc

--- RisTan/src/gameLogic/GameLogicException.java
+++ RisTan/src/gameLogic/GameLogicException.java
@@ -2,14 +2,25 @@ package gameLogic;
 
 import config.Config;
 
-//An exception from the internal game logic
+/**
+ * An exception from the internal game logic.
+ * @author Andras
+ *
+ */
 public class GameLogicException extends Exception {
 	private static final long serialVersionUID=1L;
 	
+	/**
+	 * Constructor.
+	 */
 	GameLogicException() {
 		super(Config.Exception.GameLogic.errorMessage);
 	}
 	
+	/**
+	 * Constructor with message.
+	 * @param message the message of the exception.
+	 */
 	GameLogicException(String message) {
 		super(message);
 	}
--- RisTan/src/gameLogic/TileIsNotVillageException.java
+++ RisTan/src/gameLogic/TileIsNotVillageException.java
@@ -2,10 +2,17 @@ package gameLogic;
 
 import config.Config;
 
-//a player tries an action on a not village tile which requires a village tile
+/**
+ * An exception when a player tries an action on a not village tile which requires a village tile.
+ * @author Andras
+ *
+ */
 public class TileIsNotVillageException extends GameLogicException {
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Constructor.
+	 */
 	TileIsNotVillageException(){
 		super(Config.Exception.TileIsNotVillage.errorMessage);
 	}
