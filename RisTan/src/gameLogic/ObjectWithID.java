856b35e051315ea40dada357edec1f1723c56a0d
PAndras96 <palkoandras96@gmail.com>
2018-05-01 14:49:02 +0200

    Added Javadoc

--- RisTan/src/gameLogic/ObjectWithID.java
+++ RisTan/src/gameLogic/ObjectWithID.java
@@ -1,20 +1,41 @@
 package gameLogic;
 
-//an object which has an automatically incrementing ID
+/**
+ * An object which has an automatically incrementing ID. The IDs can be set manually, but in this case the uniqueness of the IDs is not guaranteed.
+ * @author Andras
+ *
+ */
 abstract class ObjectWithID {
+	/**
+	 * The ID of the next object.
+	 */
 	private static int nextID=0;
+	/**
+	 * Gets the next ID and automatically increments it.
+	 * @return the current next ID.
+	 */
 	private static int getNextID() {
 		int ret=nextID;
 		nextID++;
 		return ret;
 	}
 	
+	/**
+	 * The ID of the object.
+	 */
 	private int ID;
 	
+	/**
+	 * Constructor, uses the automatically incrementing ID.
+	 */
 	ObjectWithID(){
 		ID=getNextID();
 	}
 	
+	/**
+	 * Constructor with arbitrary ID. If this is used, the uniqueness is not guaranteed.
+	 * @param ID the ID of the object.
+	 */
 	ObjectWithID(int ID){
 		this.ID=ID;
 		if(ID>=nextID) {
@@ -22,6 +43,10 @@ abstract class ObjectWithID {
 		}
 	}
 	
+	/**
+	 * Gets the ID.
+	 * @return the ID.
+	 */
 	int getID() {
 		return ID;
 	}
