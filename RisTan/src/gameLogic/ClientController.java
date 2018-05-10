07eae894c03d5f30e6fdd470b4d1b1e512822f4e
PAndras96 <palkoandras96@gmail.com>
2018-05-10 19:58:41 +0200

    Added Javadoc

--- RisTan/src/gameLogic/ClientController.java
+++ RisTan/src/gameLogic/ClientController.java
@@ -6,19 +6,42 @@ import network.Message;
 import network.SerialClient;
 import network.Message.eMsgType;
 
+/**
+ * A controller which handles the client side game state.
+ * @author Andras
+ *
+ */
 public class ClientController extends Controller {
+	/**
+	 * The network client for communication.
+	 */
 	private SerialClient client;
+	/**
+	 * The ID of the local player.
+	 */
 	private int localPlayerID;
 	
+	/**
+	 * Constructor.
+	 * @param client the network client.
+	 */
 	public ClientController(SerialClient client) {
 		this.client=client;
 		localPlayerID=-1;
 	}
 	
+	/**
+	 * Gets the local player ID.
+	 * @return the local plyaer ID.
+	 */
 	public int getLocalPlayerID() {
 		return localPlayerID;
 	}
 	
+	/**
+	 * Sets the local player ID.
+	 * @param localPlayerID the new local player ID.
+	 */
 	public void setLocalPlayerID(int localPlayerID) {
 		this.localPlayerID=localPlayerID;
 	}
@@ -33,7 +56,9 @@ public class ClientController extends Controller {
 		return "";
 	}
 	
-	//execute broadcasted actions from server
+	/**
+	 * Executes an action broadcasted by the server. This is the end of the action execution chain.
+	 */
 	@Override
 	public void executeAction(int playerID,Action action) {
 		try {
@@ -44,11 +69,18 @@ public class ClientController extends Controller {
 		}
 	}
 	
-	//send an action to the server for check and execution (call this function to start the execution chain)
+	/**
+	 * Sends an action to the server for checking and execution. This is the start of the action execution chain.
+	 * @param action the action to execute.
+	 */
 	public void sendAction(Action action) {
 		client.Send(new Message(eMsgType.Action,action));
 	}
 	
+	/**
+	 * Gets whether the local player is the active or not.
+	 * @return true if and only if the local player is the active.
+	 */
 	public boolean isActivePlayer() {
 		Player active=getGameState().getActivePlayer();
 		if(active==null) {
@@ -56,6 +88,4 @@ public class ClientController extends Controller {
 		}
 		return active.getID()==localPlayerID;
 	}
-	
-	//TODO: GUI eseményekbõl Action-ök elõállítása (nem feltétlenül itt)
 }
--- RisTan/src/gameLogic/GameState.java
+++ RisTan/src/gameLogic/GameState.java
@@ -5,28 +5,64 @@ import java.util.List;
 
 import config.Config;
 
-//stores the state of the game
+/**
+ * Stores the current state of the game.
+ * @author Andras
+ *
+ */
 public class GameState {
+	/**
+	 * True when the game is running, false before and after the game.
+	 */
 	private boolean over;
+	/**
+	 * True after the game, false otherwise.
+	 */
 	private boolean finished;
+	/**
+	 * The game board.
+	 */
 	private Board board;
+	/**
+	 * The players of the game.
+	 */
 	private List<Player> players;
+	/**
+	 * Handles the in game order of the players.
+	 */
 	private PlayerOrder playerOrder;
+	/**
+	 * Handles the different turns in the game.
+	 */
 	private TurnOrder turnOrder;
+	/**
+	 * Handles the trade offers in the game.
+	 */
 	private Market market;
+	/**
+	 * True if the automatic actions are executed.
+	 */
 	private boolean automaticActionsExecuted;
 	
+	/**
+	 * Constructor.
+	 */
 	public GameState() {
 		over=true;
 		board=null;
 		players=new ArrayList<Player>();
 		playerOrder=null;
 		turnOrder=null;
-		market=new Market();
+		market=null;
 		automaticActionsExecuted=false;
 		finished=false;
 	}
 	
+	/**
+	 * Tries to execute an action. If unsuccessful, an according GameLogicException is thrown and the game state remains unchanged. If successful, also checks for the obligatory events, and if the active player cannot do more in this thur, switches to the next player.
+	 * @param action the action to execute.
+	 * @throws GameLogicException if the execution is unsuccessful, an according exception is thrown.
+	 */
 	public void executeAction(Action action)throws GameLogicException{
 		action.execute(this);
 		if(!over) {
@@ -37,22 +73,42 @@ public class GameState {
 		}
 	}
 	
+	/**
+	 * Gets whether the game is over.
+	 * @return false if and only if the game is running.
+	 */
 	public boolean isOver() {
 		return over;
 	}
 	
+	/**
+	 * Gets whether the game is finished.
+	 * @return true if and only if the game is finished.
+	 */
 	public boolean isFinished() {
 		return finished;
 	}
 	
+	/**
+	 * Gets the board.
+	 * @return the board.
+	 */
 	public Board getBoard() {
 		return board;
 	}
 	
+	/**
+	 * Gets the players.
+	 * @return the list of players.
+	 */
 	List<Player> getPlayers(){
 		return players;
 	}
 	
+	/**
+	 * Gets the active player.
+	 * @return the active player or null if the game has not started.
+	 */
 	public Player getActivePlayer() {
 		if(playerOrder==null) {
 			return null;
@@ -60,6 +116,11 @@ public class GameState {
 		return playerOrder.getActive();
 	}
 	
+	/**
+	 * Gets a player by ID.
+	 * @param ID the ID of the searched player.
+	 * @return the searched player or null if there isn't any player with this ID.
+	 */
 	Player getPlayerByID(int ID) {
 		for(int i=0;i<players.size();++i) {
 			Player player=players.get(i);
@@ -70,29 +131,53 @@ public class GameState {
 		return null;
 	}
 	
+	/**
+	 * Gets the current turn.
+	 * @return the current turn.
+	 */
 	public Turn getTurn() {
+		if(turnOrder==null) {
+			return null;
+		}
 		return turnOrder.getActive();
 	}
 	
+	/**
+	 * Gets the market.
+	 * @return the market.
+	 */
 	public Market getMarket() {
 		return market;
 	}
 	
+	/**
+	 * Gets the list of possible tile actions.
+	 * @return the list of possible tile actions.
+	 */
 	public List<TileAction> getPossibleTileActions(){
 		return getTurn().getPossibleTileActions(this);
 	}
 	
+	/**
+	 * Inits the game. Generates a new board, a new player order and loads the turn order. To be called on the server.
+	 * @throws GameLogicException if an automatic action of the first player is invalid.
+	 */
 	void initGame() throws GameLogicException {
 		board=new Board();
 		board.generate(Config.Board.res_prob);
 		playerOrder=new PlayerOrder(players);
 		turnOrder=new TurnOrder(Config.TurnOrder.turns);
+		market=new Market();
 		over=false;
 		finished=false;
 		//effectively starting the game
 		activePlayerStart();
 	}
 	
+	/**
+	 * Gets a start game action from which the clients can recreate the starting state of the game. To be called on the server, after the initGame().
+	 * @return the start game action which needs to be executed on the clients.
+	 */
 	public StartGameAction getStartGameAction() {
 		String boardGenerator=board.getGeneratorString();
 		String playerShuffleOrder=playerOrder.getShuffleOrder();
@@ -100,12 +185,20 @@ public class GameState {
 		return new StartGameAction(boardGenerator,playerShuffleOrder,turnOrderGenerator);
 	}
 	
+	/**
+	 * 
+	 * @param boardGenerator
+	 * @param playerShuffleOrder
+	 * @param turnOrderGenerator
+	 * @throws GameLogicException
+	 */
 	void startGame(String boardGenerator,String playerShuffleOrder,String turnOrderGenerator) throws GameLogicException{
 		if(over) {//true on clients, needs to generate game state according to server
 			board=new Board();
 			board.generate(boardGenerator);
 			playerOrder=new PlayerOrder(players,playerShuffleOrder);
 			turnOrder=new TurnOrder(turnOrderGenerator);
+			market=new Market();
 			over=false;
 			finished=false;
 		}
